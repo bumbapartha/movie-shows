b563ef0662271a5bc1675d791f879a2e3f79dfb2
consultant <consultant@192.168.0.104>
2022-01-14 16:27:50 +0530

    Initial Checkin

---
+++ src/test/java/com/movie/movieshows/MovieShowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.movieshows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieShowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
